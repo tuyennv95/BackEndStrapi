7a77e75939d0dddc5081e02a6f63f69560a9319e
tuyennguyen <tuyennv.1995@gmail.com>
2021-01-27 14:30:10 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4b8a93a8500d13f7c079317259eb8296'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a262662d-228a-4bb9-b79c-b293444d4324'
+};
